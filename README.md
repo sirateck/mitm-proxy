1251cf5813e160d88177445b990c3c2caad51d08
Kassim Belghait <kassim@sirateck.com>
2022-02-04 21:33:48 +0100

    update install.sh Add Readme.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# Mitm proxy
+
+# Install on debian
+
+`curl -s https://raw.githubusercontent.com/sirateck/mitm-proxy/master/install.sh | sudo bash`
--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/bin/env bash
 # Install nodejs 16x Using Debian, as root
 NODE_MAJOR_VERSION=16.x
 curl -fsSL https://deb.nodesource.com/setup_$NODE_MAJOR_VERSION | bash -
@@ -9,4 +10,10 @@ npm install --global yarn
 # install pm2 to demonize the proxy script
 yarn global add pm2
 
-git clone 
+# clone proxy script
+git clone git@github.com:sirateck/mitm-proxy.git
+
+cd mitm-proxy
+
+# start proxy application
+pm2 start index.ts
