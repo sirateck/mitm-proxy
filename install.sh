1c85a1084bef5877a06684b40078789ba3c6626f
Kassim Belghait <kassim@sirateck.com>
2022-02-04 23:42:50 +0100

    Remove os dependencies from install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/env bash
 # Install nodejs 16x Using Debian, as root
-NODE_MAJOR_VERSION=16.x
-curl -fsSL https://deb.nodesource.com/setup_$NODE_MAJOR_VERSION | bash -
-apt-get update && apt-get install -y nodejs git
+# NODE_MAJOR_VERSION=16.x
+# curl -fsSL https://deb.nodesource.com/setup_$NODE_MAJOR_VERSION | bash -
+# apt-get update && apt-get install -y nodejs git
 
 # install yarn
 npm install --global yarn
